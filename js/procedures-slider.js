b388ebc2e931071da0ef747a5d20412619d2ee29
Artur Lisetskyi <126811047+weaaves@users.noreply.github.com>
2023-09-04 23:23:04 +0300

    Add files via upload

---
+++ js/procedures-slider.js
@@ -0,0 +1,23 @@
+const swiper = new Swiper('.swiper-container', {
+  loop: true,
+  slidesPerView: 1,
+  spaceBetween: 20,
+  breakpoints: {
+    768: {
+      slidesPerView: 2,
+    },
+    // when window width is >= 640px
+    1280: {
+      slidesPerView: 3,
+    }
+  },
+  pagination: {
+    el: '.pagination',
+    bulletClass: 'pagination__button',
+    bulletActiveClass: 'pagination__button--active',
+  },
+  navigation: {
+    nextEl: '.carousel-button.next',
+    prevEl: '.carousel-button.prev',
+  },
+});
